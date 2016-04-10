aee4d84cc304cefc273965501b8c8ca553efef79
olga <Cherepovskaya@gmail.com>
2016-04-10 16:53:47 +0300

    error message and somsingsomsing

--- index.php
+++ index.php
@@ -5,28 +5,47 @@
  * Date: 09.04.16
  * Time: 11:51
  */
-include_once("model.php");
-include_once ("template.php");
-$errors = [];
+$data = array(
+    array(
+      'name'=>'1N',
+      'email' =>'2N',
+      'date'=> date("Y-m-d"),
+      'message' => '3N'
+    ),
+    array(
+        'name'=>'1o',
+        'email' =>'2o',
+        'date'=> date("Y-m-d"),
+        'message' => '3o'
+    ),
+    array(
+        'name'=>'1ot',
+        'email' =>'2ot',
+        'date'=> date("Y-m-d"),
+        'message' => '3ot'
+    ),
+);
+//include_once("model.php");
+//include_once ("template.php");
+$error = [];
 
 //функция на валидацию данных от пользователя и сохранение вбазу данных
-if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['massage'])){
-    $email = $_POST['email'];
-    $name = $_POST['name'];
-    $massage = $_POST['massage'];
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-        echo "<h1 style='text-align: center;'>Все верно, данные валидны!</h1>";
-        $errors[] = "Email "
-
-    }
-
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['massage']) && !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false){
+        echo "<h3>Данные будут сохранены </h3>";
+        $date = array(
+            'email' => $_POST['email'],
+            'name' =>  $_POST['name'],
+            'message' => $_POST['message'],
+        );
 }else{
-    $errors[] = "Все поля должны быть заполнены!";
+    $error["notdata"] = "Поля введены не верно";
 }
+require_once ("template.php");
+
 
 //из базы данных вывод на страницу
 
-$a = load();
+//$data = load();
 
 
 
