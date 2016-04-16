ffff44aeada9bdf84f3515c1ee38b5636746c15c
Igor Kazankov <11igor77@mail.ru>
2016-04-16 11:09:11 +0300

    create file model

--- tests/unit/ModelTest.php
+++ tests/unit/ModelTest.php
@@ -1,7 +1,28 @@
 <?php
 
+require_once "model.php";
+
 class ModelTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Pixie\QueryBuilder\QueryBuilderHandler
+     */
+    protected $qb;
+
+    /**
+     * @var array
+     */
+    protected $config;
+
+    public function __construct($name = null, array $data = array(), $dataName = '')
+    {
+        $this->config = include "config.php";
+        $connection = new \Pixie\Connection('mysql', $this->config);
+        $this->qb = new \Pixie\QueryBuilder\QueryBuilderHandler($connection);
+
+        parent::__construct($name, $data, $dataName);
+    }
+
     protected function setUp()
     {
     }
@@ -11,8 +32,26 @@ class ModelTest extends \PHPUnit_Framework_TestCase
     }
 
     // tests
-    public function testMe()
+    public function testConfig()
     {
+
+        $this->assertArrayHasKey('username', $this->config);
         $this->assertTrue(true);
+
+    }
+
+    public function testSave(){
+
+    }
+
+    public function testSaveWithError(){
+
+    }
+
+    public function testLoad(){
+
+        $result = load($this->qb);
+        $this->assertGreaterThan(0, count($result));
+
     }
 }
