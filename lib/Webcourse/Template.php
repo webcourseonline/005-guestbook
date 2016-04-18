c7e0ba71fd9ba92c3a4ac0a5be065d6f08506976
raspberrymg <raspberry.mg@gmail.com>
2016-04-19 00:47:02 +0300

    template

---
+++ lib/Webcourse/Template.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: margarita
+ * Date: 18.04.16
+ * Time: 21:37
+ */
+
+namespace Webcourse;
+
+
+class Template
+{
+
+    /**
+     *Template render.
+     */
+    public function render()
+    {
+    }
+}
