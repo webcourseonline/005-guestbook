a2edd5c9b2bf1e22c1055cc4a89241194963fd3a
Elena Kashevarova <kashevarova.e.a@gmail.com>
2016-04-19 22:15:25 +0300

    add route method

--- lib/Webcourse/Router.php
+++ lib/Webcourse/Router.php
@@ -9,13 +9,51 @@
 namespace Webcourse;
 
 
+class ActivityStreamController
+{
+    public function addActivity()
+    {
+        echo 'work!!!';
+    }
+}
+
 class Router
 {
-    
-    /**
-     * FrontController constructor.
-     */
-    public function __construct()
+    public function route($url)
     {
+        $path = $this->dispatchUrl($url);
+        $this->callController($path);
     }
+
+    protected function dispatchUrl($url)
+    {
+        $partsOfUrl = preg_split("/[\/]+/", $url);
+
+        if ( isset($partsOfUrl[2])){
+            $controllerName = ucfirst($partsOfUrl[2]).'Controller';
+        } else {
+            $controllerName = 'IndexController';
+        }
+        if ( isset($partsOfUrl[3])){
+            $actionName = $partsOfUrl[3].'Action';
+        } else {
+            $actionName = 'indexAction';
+        }
+
+        return ['controllerName' => $controllerName,
+            'actionName' => $actionName,
+        ];
+    }
+
+    protected function callController($path)
+    {
+        $controller = new $path['controllerName']();
+        $controller->$path['actionName']();
+    }
+}
+
+
+
+$router = new Router();
+$router->route('http://domain.ru/activityStream/addActivity');
 }
