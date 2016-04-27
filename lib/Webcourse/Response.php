3e534f9e4dac55056094de37cf493813f415306e
sergii <serg.bem@gmail.com>
2016-04-27 22:16:40 +0300

    add test  Code

--- lib/Webcourse/Response.php
+++ lib/Webcourse/Response.php
@@ -12,23 +12,27 @@ namespace Webcourse;
 class Response
 {
     /**
-     * @var int
+     * @var int 
      */
     protected $code;
     /**
      * @var array
      */
-    protected $headers;
+    protected $headers = array();
     /**
      * @var array
      */
-    protected $cookies;
+    protected $cookies = array();
     /**
      * @var string
      */
-    protected $contend;
+    protected $content;
 
 
+
+    public function send(){
+        
+    }
 
     /**
      * ResponseController constructor.
@@ -36,4 +40,71 @@ class Response
     public function __construct()
     {
     }
+
+    /**
+     * @return int
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param int $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+        
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @param array $headers
+     */
+    public function setHeaders($headers)
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCookies()
+    {
+        return $this->cookies;
+    }
+
+    /**
+     * @param array $cookies
+     */
+    public function setCookies($cookies)
+    {
+        $this->cookies = $cookies;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+    
+    
 }
