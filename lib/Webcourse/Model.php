ca7a6fa90008a36ef58e37705d2a7ca7fd54b959
olga <Cherepovskaya@gmail.com>
2016-04-27 19:49:59 +0300

    _25_04_1

--- lib/Webcourse/Model.php
+++ lib/Webcourse/Model.php
@@ -9,30 +9,8 @@
 namespace Webcourse;
 use Pixie\QueryBuilder\QueryBuilderHandler;
 
+
 class Model
-$config = array(
-    'driver'    => 'mysql', // Db driver
-    'host'      => '127.0.0.1',
-    'database'  => 'guestbook',
-    'username'  => 'root',
-    'password'  => '21609332',//'test123',
-    'charset'   => 'utf8', // Optional
-    'collation' => 'utf8_general_ci', // Optional
-//    'prefix'    => '', // Table prefix, optional
-//    'options'   => array( // PDO constructor options, optional
-//        PDO::ATTR_TIMEOUT => 5,
-//        PDO::ATTR_EMULATE_PREPARES => false,
-//    ),
-
-);
-$post = array(
-    'name' => 'Olga1',
-    'email' => 'olga@gmail.com',
-    'date' => '2016-04-25',
-    'message' => 'Hello my friend!=)',
-);
-
-class FModel
 {
     public function __construct(){
 
--- tests/framework/ModelTest.php
+++ tests/framework/ModelTest.php
@@ -8,6 +8,29 @@ class ModelTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
+        //class Moded
+//$config = array(
+//    'driver'    => 'mysql', // Db driver
+//    'host'      => '127.0.0.1',
+//    'database'  => 'guestbook',
+//    'username'  => 'root',
+//    'password'  => '21609332',//'test123',
+//    'charset'   => 'utf8', // Optional
+//    'collation' => 'utf8_general_ci', // Optional
+////    'prefix'    => '', // Table prefix, optional
+////    'options'   => array( // PDO constructor options, optional
+////        PDO::ATTR_TIMEOUT => 5,
+////        PDO::ATTR_EMULATE_PREPARES => false,
+////    ),
+//
+//);
+//$post = array(
+//    'name' => 'Olga1',
+//    'email' => 'olga@gmail.com',
+//    'date' => '2016-04-25',
+//    'message' => 'Hello my friend!=)',
+//);
+
     }
 
     protected function tearDown()
