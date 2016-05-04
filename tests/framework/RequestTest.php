af71700e8dc39d34890b1da5591cea24c604146e
Roman Khukhryanskiy <r.khukhryanskiy@gmail.com>
2016-05-04 21:19:30 +0300

    request test_2

--- tests/framework/RequestTest.php
+++ tests/framework/RequestTest.php
@@ -16,8 +16,7 @@ class RequestTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf("\Webcourse\Request", $request);
     }
 
-    public function testGetParams()
-    {
+    public function testGetParams(){
         $request = new \Webcourse\Request();
         $result = $request->getParams();
         $this->assertInternalType("array", $result);
@@ -35,15 +34,33 @@ class RequestTest extends \PHPUnit_Framework_TestCase
         $this->assertInternalType("array", $result);
     }
 
+    public function testAddHeaders(){
+        $request = new \Webcourse\Request();
+        $result = $request->addHeaders(array("paramName" => "paramY"));
+        $this->assertEquals(array("paramName" => "paramY"), $result);
+    }
+
     public function testGetCookies(){
         $request = new \Webcourse\Request();
         $result = $request->getCookies();
         $this->assertInternalType("array", $result);
     }
 
+    public function testSetCookies(){
+        $request = new \Webcourse\Request();
+        $result = $request->setCookies("jsdhfj");
+        $this->assertEquals("jsdhfj", $result);
+    }
+
     public function testGetType(){
         $request = new \Webcourse\Request();
         $result = $request->getType();
         $this->assertInternalType("string", $result);
+    }
+
+    public function testSetType(){
+        $request = new \Webcourse\Request();
+        $result = $request->setType("sgsg");
+        $this->assertEquals("sgsg", $result);
     }
 }
