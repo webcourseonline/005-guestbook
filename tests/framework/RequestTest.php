71253408144e11160c256438a82766f6dbf89647
Roman Khukhryanskiy <r.khukhryanskiy@gmail.com>
2016-05-04 20:58:02 +0300

    request test

--- tests/framework/RequestTest.php
+++ tests/framework/RequestTest.php
@@ -16,25 +16,34 @@ class RequestTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf("\Webcourse\Request", $request);
     }
 
-    public function testGetParams(){
+    public function testGetParams()
+    {
         $request = new \Webcourse\Request();
         $result = $request->getParams();
-        $this->assertFalse($result);
-
-        $request->addParams(array("paramName"=>"paramX"))
+        $this->assertInternalType("array", $result);
     }
 
-    public function testTypeHeaders(){
-
+    public function testAddParams(){
+        $request = new \Webcourse\Request();
+        $result = $request->addParams(array("paramName" => "paramX"));
+        $this->assertEquals(array("paramName" => "paramX"), $result);
     }
 
-    public function testTypeCookies(){
-
+    public function testGetHeaders(){
+        $request = new \Webcourse\Request();
+        $result = $request->getHeaders();
+        $this->assertInternalType("array", $result);
     }
 
-    public function testTypeType(){
-
+    public function testGetCookies(){
+        $request = new \Webcourse\Request();
+        $result = $request->getCookies();
+        $this->assertInternalType("array", $result);
     }
 
+    public function testGetType(){
+        $request = new \Webcourse\Request();
+        $result = $request->getType();
+        $this->assertInternalType("string", $result);
+    }
 }
-
