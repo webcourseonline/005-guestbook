3ee847cd70662f1e8f7caae733d9e3df0b7d582e
Taras Omelianenko <t.omelianenko@apibest.com>
2016-05-07 21:01:55 +0300

    Update test

--- config/config_test.php
+++ config/config_test.php
@@ -1,8 +1,18 @@
 <?php
 
 $config['database'] = array(
+    'driver'    => 'mysql', // Db driver
+    'host'      => '127.0.0.1',
+    'database'  => 'test',//test
     'username'  => 'root',
     'password'  => '',
+    'charset'   => 'utf8', // Optional
+    'collation' => 'utf8_general_ci', // Optional
+    'prefix'    => '', // Table prefix, optional
+    'options'   => array( // PDO constructor options, optional
+        PDO::ATTR_TIMEOUT => 5,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ),
 );
 
 $parentConfig = include "config_dev.php";
