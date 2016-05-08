00237decba3d3dd9e32e50ab78c1c806dddaea71
Elena Kashevarova <kashevarova.e.a@gmail.com>
2016-05-08 16:22:33 +0300

    Taras chenges frontController

--- www/index.php
+++ www/index.php
@@ -5,7 +5,8 @@ require_once realpath($dir . "/../vendor/autoload.php");
 
 date_default_timezone_set('America/Los_Angeles');
 $configPath = realpath($dir . "/../config");
+$srcPath = realpath($dir . "/../src");
 
-$app = new \Webcourse\FrontController($configPath);
+$app = new \Webcourse\FrontController($configPath, \Webcourse\FrontController::ENV_DEV, $srcPath);
 $app->init();
 $app->run();
