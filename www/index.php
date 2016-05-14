5c73bf528b56e811aac71ebe17ed37bd162337eb
raspberrymg <raspberry.mg@gmail.com>
2016-05-14 11:23:58 +0300

    add & commit branch app_view

--- www/index.php
+++ www/index.php
@@ -1,6 +1,7 @@
 <?php
 
 $dir = __DIR__;
+//include __DIR__.'/../src/App/View/guestbook/about.phtml';
 require_once realpath($dir . "/../vendor/autoload.php");
 
 date_default_timezone_set('America/Los_Angeles');
